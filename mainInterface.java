8035252d50a88cf0913d4f3135e705843cb64717
Quang Tran <qut222@lehigh.edu>
2021-04-13 22:44:09 +0700

    method to encrypt a password

--- mainInterface.java
+++ mainInterface.java
@@ -1,14 +1,18 @@
 import java.security.*;
 import java.util.Base64;
 import java.util.Scanner;
-public class main{
+public class mainInterface{
   public static void main(String[] args){
     String d1 = null;
     Scanner scan = new Scanner(System.in);
-    do{
-    System.out.println("Do you want to generate key? (y/n)");
-    d1 = scan.nextLine();
-    }while(!d1.equals("y")||!d1.equals("n"));
+    boolean check1 = true;
+    while(check1){
+      System.out.println("Do you want to generate key? (y/n)");
+      d1 = scan.nextLine();
+      if(d1.equals("y") || d1.equals("n")){
+        check1 = false;
+      }
+    }
 
     if(d1.equals("y")){
     createKeys genKeys = new createKeys();
@@ -16,6 +20,6 @@ public class main{
     System.out.println(Base64.getEncoder().encodeToString(genKeys.getPublicKey().getEncoded()));
     System.out.println(Base64.getEncoder().encodeToString(genKeys.getPrivateKey().getEncoded()));
     }
-
+    scan.close();
   }
 }
